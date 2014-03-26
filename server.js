b3e78a2f02a9f557f6e0fcd0080b41e4013f92cb
Brian Han <brianhan87usa@gmail.com>
2014-03-25 23:05:23 -0400

    Require url module

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require("http");
+var url = require("url");
 
 function start() {
   function onRequest(request, response) {
+
     console.log("Request received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
