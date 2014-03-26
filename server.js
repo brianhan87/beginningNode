5918e5b76b33759f70217992f5e876266bb0a5e2
Brian Han <brianhan87usa@gmail.com>
2014-03-25 23:17:24 -0400

    Corrected typo in console.log

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require("url");
 function start(route) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + received);
+    console.log("Request for " + pathname + " received");
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
